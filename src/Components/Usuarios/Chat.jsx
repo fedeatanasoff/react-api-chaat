77bc85367240b6c2806a6106396c6f7776ebea4f
fedeatanasoff <fede.atanasoff@gmail.com>
2019-03-27 20:14:28 -0300

    corrigiendo errores para que compile la version base

--- src/Components/Usuarios/Chat.jsx
+++ src/Components/Usuarios/Chat.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import io from "socket.io-client";
 const socket = io("http://localhost:5000");
-import "./Chat.css";
-
-let random = Math.floor(Math.random() * 5) + 1;
 
 class Chat extends Component {
   state = {};
@@ -13,7 +10,11 @@ class Chat extends Component {
   componentWillMount() {}
 
   render() {
-    return <div>usuarios</div>;
+    return (
+      <div>
+        <h2>chat</h2>{" "}
+      </div>
+    );
   }
 }
 
